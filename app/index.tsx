dcdf38433fa27396b68bb0dcf6dc6dcddc53264b
peterpaularetex-creator <peterpaul.aretex@gmail.com>
2026-02-25 12:32:43 +1100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Hello Expo.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#393939",
+  },
+  text: {
+    color: "#ffff",
+  },
+});
