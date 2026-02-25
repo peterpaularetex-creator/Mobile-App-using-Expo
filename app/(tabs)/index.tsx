c96b6feb953e4acfacdc8466d1ffb48132a88158
peterpaularetex-creator <peterpaul.aretex@gmail.com>
2026-02-25 17:17:32 +1100

    Build a screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from 'react-native';
+
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Hello World.</Text>
-      <Link href="/about" style={styles.button}>
-      Go to About Screen
-      </Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -17,16 +22,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#393939",
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
-  text: {
-    color: "#ffff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#ffff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
